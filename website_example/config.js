e83c5cd73367d87e2d822ff91f07a7861ae8d1da
ParsiCoin <parsicoin@gmail.com>
2019-07-10 07:59:48 +0430

    initial commit

---
+++ website_example/config.js
@@ -0,0 +1,13 @@
+var api = "http://pars.yourwebsite.com:8117";
+
+var email = "support@pars.yourwebsite.com";
+var telegram = "https://t.me/YourPool";
+var discord = "https://discordapp.com/invite/YourPool";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://explorer.parsicoin.net/?hash={id}#blockchain_block";
+var transactionExplorer = "https://explorer.parsicoin.net/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
